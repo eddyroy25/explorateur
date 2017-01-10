2659c63abddf0645038a4c60a100a2afaa93638c
Unknown <eddyroy2125@gmail.com>
2017-01-10 16:20:22 +0100

    Add functions to explorer

--- explorateur php serveur/explorateur.php
+++ explorateur php serveur/explorateur.php
@@ -22,36 +22,32 @@ $dirs = scandir($adresse);
 	
 foreach ($dirs as $folder) {
 	
-	if (is_file("/home/eddyr/$folder")) {
+	if (!is_dir("/home/eddyr/$folder")) {
 		
 		if ($folder == ".."){
 		
-		echo "<img src='images/folder.png'><a href='explorateur.php'><?=$folder?></a><br>";
+			echo "<img src='images/folder.png'><a href='explorateur.php'>$folder</a><br>";
 					
-		// echo "<img src='images/file.png'><a href='/home/eddyr/$folder/'>$folder</a><br>";
-	
-	
 		}
 		else {
 			
-				if (isset($_GET['dossier'])) {
+			if (isset($_GET['dossier'])) {
 					
-					echo '<img src="images/folder.png"><a href="explorateur.php?dossier=<?='.$_GET["dossier"].'?><?=$folder?>/"><?=$folder?></a><br>';
+				echo "<img src='images/folder.png'><a href='explorateur.php?dossier='".$_GET['dossier']."$folder/>$folder</a><br>";
 			
-			// echo "<img src='images/folder.png'><a href='/home/eddyr/$folder/'>$folder</a><br>";
-				}
+			}
 	
-				else {
+			else {
 					
-				echo "<img src='images/file.png'><a href='explorateur.php?dossier=<?=$folder?>/'><?=$folder?></a><br>";
+				echo "<img src='images/file.png'><a href='explorateur.php?dossier=$folder/'>$folder</a><br>";
 				
-				}
+			}
 	
 		}
 	}
 		
 	else {
-		echo "<img src='images/folder.png'><a href='explorateur.php?dossier=<?=$folder?>/'>$folder</a><br>";
+		echo "<img src='images/folder.png'><a href='explorateur.php?dossier=$folder/'>$folder</a><br>";
 	}
 
 }
